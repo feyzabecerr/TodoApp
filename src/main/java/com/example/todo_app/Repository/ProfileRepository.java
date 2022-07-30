d0eac9cfdf0e7a47a0882299ae079f21870953fc
feyzabecerr <19120205036@ismu.edu.tr>
2022-07-30 22:31:27 +0300

    postgres added

--- src/main/java/com/example/todo_app/Repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.todo_app.Repository;
-
-import com.example.todo_app.Entity.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile,Long>
-{
-
-}
