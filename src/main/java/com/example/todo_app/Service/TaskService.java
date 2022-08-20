6ca5c67d1407d60473f1e47b664c6fc61b4c3bae
feyzabecerr <19120205036@ismu.edu.tr>
2022-08-21 01:42:22 +0300

    service layer added

--- src/main/java/com/example/todo_app/Service/TaskService.java
+++ src/main/java/com/example/todo_app/Service/TaskService.java
@@ -1,2 +1,66 @@
-package com.example.todo_app.Service;public class TaskService {
+package com.example.todo_app.Service;
+
+import com.example.todo_app.DTO.UserTaskDTO;
+import com.example.todo_app.Entity.Task;
+import com.example.todo_app.Entity.User;
+import com.example.todo_app.Repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    private UserService userService;
+
+    public TaskService(TaskRepository taskRepository, UserService userService) {
+        this.taskRepository = taskRepository;
+        this.userService = userService;
+    }
+
+    public List<Task> getAllTasks(Optional<Long> userId) {
+        if(userId.isPresent())
+            return taskRepository.findByUserId(userId.get());
+
+        return taskRepository.findAll();
+    }
+
+    public Task getOneTask(Long taskId) {
+        return taskRepository.findById(taskId).
+                orElseThrow(() -> new IllegalStateException("task with " + taskId + " doesn't exist"));
+    }
+
+    public Task updateOneTask(Long taskId, UserTaskDTO userTaskDTO) {
+        Optional<Task> task = taskRepository.findById(taskId);
+        if(task.isPresent()){
+            Task toUpdate = task.get();
+            toUpdate.setDescription(userTaskDTO.getDescription());
+            System.out.println(userTaskDTO.getDescription());
+            toUpdate.setDate(userTaskDTO.getDate());
+            toUpdate.setActive(userTaskDTO.isActive());
+
+            taskRepository.save(toUpdate);
+
+            return toUpdate;
+        }
+        else{
+            throw new IllegalStateException();
+        }
+    }
+
+    public Task createOneTask(UserTaskDTO userTaskDTO) {
+        User user = userService.getOneUser(userTaskDTO.getUserId());
+        if(user == null)
+            return null;
+
+        Task task = new Task();
+        task.setActive(userTaskDTO.isActive());
+        task.setDate(userTaskDTO.getDate());
+        task.setDescription(userTaskDTO.getDescription());
+        task.setUser(user);
+        return taskRepository.save(task);
+    }
 }
--- src/main/java/com/example/todo_app/Service/UserService.java
+++ src/main/java/com/example/todo_app/Service/UserService.java
@@ -1,2 +1,52 @@
-package com.example.todo_app.Service;public class UserService {
+package com.example.todo_app.Service;
+
+
+import com.example.todo_app.DTO.UserTaskDTO;
+import com.example.todo_app.Entity.User;
+import com.example.todo_app.Repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getOneUser(Long id) {
+        return  userRepository.findById(id).
+                orElseThrow(() -> new IllegalStateException("user with " + id + " doesn't exist"));
+    }
+
+    public User updateOneUser(Long id, User newUser) {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()){
+            User foundUser = user.get();
+            foundUser.setUsername(newUser.getUsername());
+            foundUser.setEmail(newUser.getEmail());
+            foundUser.setPassword(newUser.getPassword());
+            userRepository.save(foundUser);
+            return foundUser;
+        }
+        else{
+            throw new IllegalStateException();
+        }
+    }
+
+    public User getUserById(long id) {
+       User user = userRepository.findById(id).orElseThrow(() -> new IllegalStateException());
+       return user;
+    }
 }
