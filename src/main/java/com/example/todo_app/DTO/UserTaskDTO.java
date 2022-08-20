42cf58f992a268973ff00688ad4defbf1e0274c8
feyzabecerr <19120205036@ismu.edu.tr>
2022-08-21 01:39:35 +0300

    DTO added

--- src/main/java/com/example/todo_app/DTO/UserTaskDTO.java
+++ src/main/java/com/example/todo_app/DTO/UserTaskDTO.java
@@ -1,2 +1,20 @@
-package com.example.todo_app.DTO;public class UserTaskDTO {
+package com.example.todo_app.DTO;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+public class UserTaskDTO {
+
+    private boolean isActive;
+
+    private long userId;
+
+    private String description;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
 }
