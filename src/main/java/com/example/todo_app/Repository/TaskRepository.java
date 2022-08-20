ffa7b4a36570598872e6d6569e0ca60e5b3c9d3e
feyzabecerr <19120205036@ismu.edu.tr>
2022-08-21 01:43:20 +0300

    method added

--- src/main/java/com/example/todo_app/Repository/TaskRepository.java
+++ src/main/java/com/example/todo_app/Repository/TaskRepository.java
@@ -3,6 +3,8 @@ package com.example.todo_app.Repository;
 import com.example.todo_app.Entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task,Long> {
+import java.util.List;
 
+public interface TaskRepository extends JpaRepository<Task,Long> {
+    List<Task> findByUserId(Long aLong);
 }
